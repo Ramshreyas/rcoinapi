4a1f2846bfb0e9274015aaa36c97b0d0d7ef9666
Ramshreyas Rao <ramshreyas.rao@gmail.com>
2021-06-26 12:21:19 +0530

    Initial Commit with utils

---
+++ R/utils.R
@@ -0,0 +1,93 @@
+
+executeRequest <- function(method, path, params = NULL, body = NULL, retries = 0) {
+
+  apiKey <- Sys.getenv("COIN_API_KEY")
+
+  if(apiKey == "") stop("No API Key!! Please set CoinAPI key with setAPIKey(<yourkeygoeshere>)")
+
+  url <- paste0(BASE_URL, path)
+  METHOD <- getFromNamespace(method, ns = 'httr')
+
+  res <- tryCatch(
+
+    METHOD(url,
+           query = params,
+           body,
+           add_headers(`X-CoinAPI-Key` = apiKey)
+    ),
+
+    error = function(e) e
+
+  )
+
+  print(res)
+
+  parseJSONResponse(res)
+
+}
+
+executeXtsRequest <- function(method, path, params = NULL, body = NULL, retries = 0, indexBy = "time_period_start") {
+
+  res <- executeRequest(method, path, params, body, retries)
+
+  xts(res, order.by = as_datetime(res[,indexBy]))
+
+}
+
+renameOHLCVColumns <- function(xtsObject, symbolName) {
+
+  if(ncol(xtsObject) == 8) {
+
+    colnames(xtsObject) <- c("time_period_start",
+                             "time_period_end",
+                             "time_open",
+                             "time_close",
+                             paste0(symbolName, ".Open"),
+                             paste0(symbolName, ".High"),
+                             paste0(symbolName, ".Low"),
+                             paste0(symbolName, ".Close"))
+
+  } else {
+
+    colnames(xtsObject) <- c("time_period_start",
+                             "time_period_end",
+                             "time_open",
+                             "time_close",
+                             paste0(symbolName, ".Open"),
+                             paste0(symbolName, ".High"),
+                             paste0(symbolName, ".Low"),
+                             paste0(symbolName, ".Close"),
+                             paste0(symbolName, ".Volume"),
+                             "trades_count")
+
+  }
+
+  xtsObject
+
+}
+
+listToStringArray <- function(l) {
+
+  stringArray <- ""
+
+  for (item in l) {
+    stringArray <- paste0(stringArray, item, ",")
+  }
+
+  substr(stringArray, 1, nchar(stringArray) - 1)
+
+}
+
+setApiKey <- function(apiKey) {
+
+  Sys.setenv(COIN_API_KEY = apiKey)
+
+}
+
+parseJSONResponse <- function(res) {
+
+  resText <- content(res, as = "text")
+
+  fromJSON(resText, flatten = TRUE)
+
+}
